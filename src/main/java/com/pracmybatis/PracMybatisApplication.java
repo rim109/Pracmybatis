a346673bbaf98552d4daa9e75ccf043bce77491a
rim109 <fla7722@naver.com>
2025-06-23 20:50:16 +0900

    first commit

---
+++ src/main/java/com/pracmybatis/PracMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.pracmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pracmybatis/PracMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pracmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
